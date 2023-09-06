2581b9600db682eac57a308a2cbe57dcf0593aac
Joseph Martire <joemart06@gmail.com>
2023-09-06 15:05:35 -0400

    Add Pages router

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import '@/styles/globals.css'
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import { ReactNode, ReactElement } from 'react'
+
+type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+
+    const getLayout = Component.getLayout ?? ((page) => page)
+
+    return getLayout(<Component {...pageProps} />)
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Spline from '@splinetool/react-spline';
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+    return <div
+        className={`relative text-[16px] color-white`}
+    >{children}</div>
+}
+
+export default function App() {
+    return (
+        <Wrapper>
+            <Spline className={`absolute margin-0 top-0 right-0`} scene="https://prod.spline.design/zbTwaL0-hYYh3vJx/scene.splinecode" />
+        </Wrapper>
+    );
+}
